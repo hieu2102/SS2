7bcd9483ac3a87c19f6321c5295fbaef914bfee4
hieu2102 <gobanme.pierro@gmail.com>
2019-03-21 18:11:15 +0700

    update twitter api funcs

--- TwitterAPI/TweetController.py
+++ TwitterAPI/TweetController.py
@@ -1,5 +1,6 @@
 # coding: utf8
 import Connect as conn
+from collections import defaultdict 
 
 user = conn.api.me()
 
@@ -11,18 +12,25 @@ def subscriptionList():
     screen_names = [user.screen_name for user in conn.api.lookup_users(user_ids=ids)]
     return screen_names
 
-### does not get full tweet length
 def getStatuses(screenName):
     print(screenName)
-    tweets = conn.api.user_timeline(screen_name="@"+screenName):
-    for tweet in tweets 
-        if (tweet.lang =="en"):
-            print ((tweet.text).encode("utf-8"))
-    return tweets
+    new_tweets = conn.api.user_timeline(screen_name = "@"+screenName, tweet_mode="extended", search_params="exclude:replies")
+    tweets = [[tweet.full_text] for tweet in new_tweets]
+    list_tweet = {}
+    for id,text in zip(new_tweets,tweets):
+        print(id.id, text)
+        tId = id.id
+        list_tweet.update({tId:text})
+    return list_tweet
 
 
 def getRetweets(tweetId):
-    retweets= conn.api.retweets(tweetId, 5)
+    retweets= conn.api.retweets(tweetId, count=5)
     for retweet in retweets:
         print(retweet.text)
     return retweets
+
+tDict = getStatuses("elonmusk")
+for entry in tDict:
+    print(tDict[entry])
+    getRetweets(entry)
