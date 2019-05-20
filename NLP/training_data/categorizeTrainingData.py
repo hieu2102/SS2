ae9c5b7704c758a7aba272e02312611e8e9834ea
hieu2102 <gobanme.pierro@gmail.com>
2019-05-20 08:10:07 +0700

    Signed-off-by: hieu2102 <gobanme.pierro@gmail.com>

--- NLP/training_data/categorizeTrainingData.py
+++ NLP/training_data/categorizeTrainingData.py
@@ -1,28 +1,120 @@
 # encoding: utf-8
+import spacy
+from nltk.stem.wordnet import WordNetLemmatizer
+from spacy.lang.en import English
+import random
+import nltk
+from nltk.corpus import wordnet as wn
+import gensim 
+from gensim import corpora
+import pickle
 
-#import io
-#import json
-#source = open("C:\\Users\\DELL\\Desktop\\SS2\\NLP\\training_data\\News_Category_Dataset_v2.json")
-#sport_file = open("sports.json", mode="w", encoding="utf-8")
-#politics_file = open("politics.json", mode = "w", encoding="utf-8")
-#food_file = open("food.json", mode = "w", encoding="utf-8")
-#technology_file = open("tech.json", mode="w", encoding="utf-8")
-#
-#for line in source:
-#    d=json.loads(line)
-#    d.pop("authors")
-#    d.pop("link")
-#    d.pop("date")
-#    if d["category"] == "POLITICS":
-#        json.dump(d,politics_file)
-#        politics_file.write("\n")
-#    if d["category"] == "TECH":
-#        json.dump(d,technology_file)
-#        technology_file.write("\n")
-#    if d["category"] == "FOOD & DRINK":
-#        json.dump(d,food_file)
-#        food_file.write("\n")
-#    if d["category"] == "SPORTS":
-#        json.dump(d,sport_file)
-#        sport_file.write("\n")
-#
+# prepare spacy parser -> get text structure 
+spacy.load('en')
+parser = English()
+# basically a dictionary
+# nltk.download('wordnet')
+# get English stopwords
+# nltk.download('stopwords')
+en_stop = set(nltk.corpus.stopwords.words('english'))
+
+# tokenize input
+def tokenize(text):
+    lda_tokens = []
+    tokens = parser(text)
+    for token in tokens:
+        if token.orth_.isspace():
+            continue
+        elif token.like_url:
+            lda_tokens.append('URL')
+        elif token.orth_.startswith('@'):
+            lda_tokens.append('SCREEN_NAME')
+        else:
+            lda_tokens.append(token.lower_)
+    return lda_tokens
+def get_lemma(word):
+    lemma = wn.morphy(word)
+    if lemma is None:
+        return word
+    else:
+        return lemma
+    
+def get_lemma2(word):
+    return WordNetLemmatizer().lemmatize(word)
+
+def prepare_text_for_lda(text):
+    tokens = tokenize(text)
+    tokens = [token for token in tokens if len(token) > 4]
+    tokens = [token for token in tokens if token not in en_stop]
+    tokens = [get_lemma(token) for token in tokens]
+    return tokens
+
+# text_data = []
+# with open('C:\\\\Users\\USER\\Desktop\\SS2\\NLP\\training_data\\dataset.csv',encoding="utf8") as f:
+#     for line in f:
+#         tokens = prepare_text_for_lda(line)
+#         if random.random() > .99:
+#             try: 
+#                 # print(tokens)
+#                 text_data.append(tokens)
+#             except:
+#                 continue
+# dictionary = corpora.Dictionary(text_data)
+# corpus = [dictionary.doc2bow(text) for text in text_data]
+# pickle.dump(corpus, open('corpus.pkl', 'wb'))
+# dictionary.save('dictionary.gensim')
+# NUM_TOPICS = 4
+# ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = NUM_TOPICS, id2word=dictionary, passes=150)
+# # ldamodel.save('model5.gensim')
+# topics = ldamodel.print_topics(num_words=10)
+# for topic in topics:
+#     print(topic)
+# new_doc = "When it comes to cakes, the more layers it has, the more impressive it feels -- that's why crepe cakes always seem so special"
+# new_doc = prepare_text_for_lda(new_doc)
+# new_doc_bow = dictionary.doc2bow(new_doc)
+# print(new_doc_bow)
+# print(ldamodel.get_document_topics(new_doc_bow))
+NUM_TOPICS = 10
+def getLargestValue(dict):
+    print(dict)
+    # print ("get most likely topic")
+    max = (dict[0])[1]
+    # print(max)
+    index =1
+    while (index <NUM_TOPICS):
+        if (dict[index])[1]> max:
+            # print (dict[index])
+            max = (dict[index])[1]
+        index=index+1
+    # print('most likely topic is: ',max)
+    return max
+cat = ['foods', 'politics', 'sports','tech']
+for c in cat:  
+    print(c)
+    text_data = []
+    with open('C:\\\\Users\\USER\\Desktop\\SS2\\NLP\\training_data\\'+c+'.csv',encoding="utf8") as f:
+        for line in f:
+            tokens = prepare_text_for_lda(line)
+            if random.random() > .99:
+                try: 
+                    # print(tokens)
+                    text_data.append(tokens)
+                except:
+                    continue
+    dictionary = corpora.Dictionary(text_data)
+    corpus = [dictionary.doc2bow(text) for text in text_data]
+    pickle.dump(corpus, open('corpus.pkl', 'wb'))
+    dictionary.save(c+'dictionary.gensim')
+    ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = NUM_TOPICS, id2word=dictionary, passes=2805)
+    ldamodel.save(c+'model5.gensim')
+    # topics = ldamodel.print_topics(num_words=10)
+    # # for topic in topics:
+    #     # print(topic)
+    # new_doc = "Watch The Top 9 YouTube Videos Of The Week,\"If you're looking to see the most popular YouTube videos of the week, look no further. Once again, we\'re bringing you the"
+    # new_doc = prepare_text_for_lda(new_doc)
+    # new_doc_bow = dictionary.doc2bow(new_doc)
+    # print(new_doc)
+    # print(new_doc_bow)
+    # topics = ldamodel.get_document_topics(new_doc_bow)
+    # # print(topics)
+    # print(getLargestValue(topics))
